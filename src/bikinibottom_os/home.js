5af50f94f3781aef694eecece57853b3ee43b0b7
Christopher Blum <christopher.blum@xing.com>
2009-07-08 15:28:57 +0200

    Created tabs

--- src/bikinibottom_os/home.js
+++ src/bikinibottom_os/home.js
@@ -2,9 +2,42 @@
  * Bikini-Bottom
  * This JavaScript file is for Home view.
  */
+var xing = xing || {};
+xing.bikinibottom = {};
 
-function init() {
-    // TODO: Write the code for initializing.
-}
-
-// TODO: Write the code for Home view.
+xing.bikinibottom.Home = {
+  tabs: {
+    "New": {
+      id: "new",
+      callback: function() {},
+      tooltip: "Create new video message"
+    },
+    
+    "Inbox": {
+      id: "inbox",
+      callback: function() {},
+      tooltip: "Received messages"
+    },
+    
+    "Outbox": {
+      id: "outbox",
+      callback: function() {},
+      tooltip: "Sent messages"
+    }
+  },
+  
+  initialize: function(settings) {
+    this._tabObj = settings.tabs;
+    this._createTabs();
+  },
+  
+  _createTabs: function() {
+    for (var tabName in this.tabs) {
+      this._tabObj.addTab(tabName, {
+        contentContainer: _gel(this.tabs[tabName].id),
+        callback: this.tabs[tabName].callback,
+        tooltip: this.tabs[tabName].tooltip
+      });
+    }
+  }
+};
