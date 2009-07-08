5f042673b04ca0c2a443cc28862045e87a8afba8
ali.jelveh <ali.jelveh@xing.com>
2009-07-08 12:36:01 +0200

    opensocial app

---
+++ src/bikinibottom_os/target/home.js
@@ -0,0 +1,10 @@
+/**
+ * Bikini-Bottom
+ * This JavaScript file is for Home view.
+ */
+
+function init() {
+    // TODO: Write the code for initializing.
+}
+
+// TODO: Write the code for Home view.
