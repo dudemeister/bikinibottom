91140b7583d5600a428e1386b6dfb315b0fafc85
Philip Oertel <phillip.oertel@xing.com>
2009-07-08 20:11:25 +0200

    setting up tab for textchat

--- src/bikinibottom_os/js/home.js
+++ src/bikinibottom_os/js/home.js
@@ -248,12 +248,17 @@ xing.bikinibottom.Home.Outbox = Class.create({
 
 xing.bikinibottom.Home.TextChat = Class.create({
   ids: {
-    CONTAINER: "text-chat"
+    CONTAINER: "text-chat",
+    FORM: "text-chat-form",
+    CONTACT_CHOOSER: "text-chat-partner",
+    MESSAGE_INPUT: "text-chat-message"
   },
   
   initialize: function(parent) {
     this.parent = parent;
     this.container = $(this.ids.CONTAINER);
+    this._initElements();
+    //this._loadData();
   },
   
   getTabData: function() {
@@ -263,6 +268,67 @@ xing.bikinibottom.Home.TextChat = Class.create({
       tooltip: "Chat", // [RES]
       callback: this._loadTab.bind(this)
     };
+  },
+    
+  _initElements: function() {
+    this._form = $(this.ids.FORM);
+    this._contactChooser = $(this.ids.CONTACT_CHOOSER);
+    this._form.getElements().invoke("disable");
+  },
+  
+  _loadData: function() {
+    console.log('_loadData')
+    var req, friendsSpec, friendsParams, ownerSpec, ownerParams;
+    
+    req = opensocial.newDataRequest();
+    
+    ownerSpec = opensocial.IdSpec.PersonId.OWNER;
+    ownerParams = {};
+    req.add(req.newFetchPersonRequest(ownerSpec, ownerParams), "owner");
+    
+    friendsSpec = opensocial.newIdSpec({ userId: "VIEWER", groupId: "FRIENDS" });
+    friendsParams = {};
+    friendsParams[opensocial.DataRequest.PeopleRequestFields.MAX] = 100;
+    req.add(req.newFetchPeopleRequest(friendsSpec, friendsParams), "friends");
+    
+    req.send(this._dataCallback.bind(this));
+  },
+  
+  _dataCallback: function(data) {
+    this._owner = data.get("owner").getData();
+    this._friends = data.get("friends").getData();
+    
+    this._renderRecipients();
+    
+    this._form.getElements().invoke("enable");
+  },
+  
+  _renderRecipients: function() {
+    var optionTemplate, optionHtml, html;
+    
+    optionTemplate = '<option value="#{id}">#{displayName}</option>';
+    html = [];
+    
+    // "each" is here not the prototype method
+    this._friends.each(function(friend) {
+      optionHtml = optionTemplate.interpolate({
+        displayName: friend.getDisplayName(),
+        id: friend.getId()
+      });
+      html.push(optionHtml);
+    });
+    
+    this._contactChooser.insert(html.join(""));
+    this._contactChooser.enable();
+    // Select first entry
+    this._contactChooser.down().selected = true;
+  },
+  
+  _observe: function() {
+    this._form.observe("submit", function(event) {
+      event.stop();
+      Dispatcher.sendMessage($(this.ids.MESSAGE_INPUT));
+    }.bind(this));
   },
   
   _loadTab: function() {
